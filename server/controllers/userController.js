3f6c47a84eed84b4da716f13379166bd7f7f4694
tylerdurden2k2 <phamquanghuy2809@gmail.com>
2024-06-22 16:15:12 +0700

    handle Error middleware

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,7 +2,7 @@ import User from "../models/User.js";
 import bcrypt from "bcryptjs";
 import generateToken from "../utils/generateToken.js";
 
-export const registerUser = async (req, res) => {
+export const registerUser = async (req, res, next) => {
 	try {
 		const { name, email, password } = req.body;
 
@@ -10,7 +10,7 @@ export const registerUser = async (req, res) => {
 		let user = await User.findOne({ email });
 
 		if (user) {
-			return res.status(400).json({ message: "User have already registered!" });
+			throw new Error("User have already registered!");
 		}
 
 		const salt = bcrypt.genSaltSync(10);
@@ -23,6 +23,7 @@ export const registerUser = async (req, res) => {
 			password: hashedPassword,
 		});
 
+		// generate token -> authenticate || authorization
 		const token = await generateToken({ id: user._id });
 
 		return res.status(201).json({
@@ -35,6 +36,6 @@ export const registerUser = async (req, res) => {
 			token,
 		});
 	} catch (error) {
-		return res.status(500).json({ message: "Something went wrong" });
+		next(error);
 	}
 };
--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 import express from "express";
 import { configDotenv } from "dotenv";
 import connectDB from "./config/db.js";
+import {
+	errorResponseHandler,
+	invalidPathHandler,
+} from "./middlewares/errorHandler.js";
 import userRoute from "./routes/userRoute.js";
 
 configDotenv();
@@ -8,8 +12,14 @@ connectDB();
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+// received request
 app.use(express.json());
 app.use("/api/users", userRoute);
+
+// handle Error
+app.use(invalidPathHandler);
+app.use(errorResponseHandler);
 
 app.listen(PORT, () => {
 	console.log("Server is running on port ", PORT);
