b9eb3f2cd8eda0c93cd404fb09d636f35493fc81
tylerdurden2k2 <phamquanghuy2809@gmail.com>
2024-06-22 11:11:16 +0700

    CTA component done

--- src/constants/images.js
+++ src/constants/images.js
@@ -2,12 +2,14 @@ import Logo from "../assets/Logo.svg";
 import HeroImage from "../assets/HeroImage.svg";
 import PostImage from "../assets/posts/post1.jpg";
 import PostProfileImage from "../assets/posts/post-profile.svg";
+import CtaImage from "../assets/CtaImage.jpg";
 
 const images = {
 	Logo,
 	HeroImage,
 	PostImage,
 	PostProfileImage,
+	CtaImage,
 };
 
 export default images;
--- src/pages/home/container/Articles.jsx
+++ src/pages/home/container/Articles.jsx
@@ -1,11 +1,19 @@
+import { FaArrowRight } from "react-icons/fa";
 import ArticleCard from "../../../components/ArticleCard";
 
 const Articles = () => {
 	return (
-		<section className="container mx-auto flex flex-wrap px-5 py-10 md:gap-5">
-			<ArticleCard className="w-full md:w-[calc(50%-20px)] lg:w-[calc(33.33%-21px)]" />
-			<ArticleCard className="w-full md:w-[calc(50%-20px)] lg:w-[calc(33.33%-21px)]" />
-			<ArticleCard className="w-full md:w-[calc(50%-20px)] lg:w-[calc(33.33%-21px)]" />
+		<section className="container mx-auto flex flex-wrap px-5 py-10 md:gap-5 ">
+			<div className="flex flex-wrap px-5 py-10 gap-5">
+				<ArticleCard className="w-full md:w-[calc(50%-20px)] lg:w-[calc(33.33%-21px)]" />
+				<ArticleCard className="w-full md:w-[calc(50%-20px)] lg:w-[calc(33.33%-21px)]" />
+				<ArticleCard className="w-full md:w-[calc(50%-20px)] lg:w-[calc(33.33%-21px)]" />
+			</div>
+
+			<button className="mx-auto flex items-center gap-x-2 font-bold text-primary border-2 border-primary px-6 py-3 rounded-lg">
+				<span>More articles</span>
+				<FaArrowRight className="w-3 h-3" />
+			</button>
 		</section>
 	);
 };
