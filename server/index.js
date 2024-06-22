c1026a0be63073bee6fa934eff5db56cec4783a8
tylerdurden2k2 <phamquanghuy2809@gmail.com>
2024-06-22 15:33:48 +0700

    connect DB

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import { configDotenv } from "dotenv";
+import connectDB from "./config/db.js";
 
 configDotenv();
+connectDB();
 const PORT = process.env.PORT || 3000;
 
 const app = express();
